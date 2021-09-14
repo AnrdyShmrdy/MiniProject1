bc02f571a538e23aeaec0bd9f9fc12f61117babc
Jman7615 <55512981+Jman7615@users.noreply.github.com>
2021-09-14 18:10:56 -0400

    comments

--- user/getCounterNum.c
+++ user/getCounterNum.c
@@ -1,6 +1,7 @@
 #include "types.h"
 #include "user.h"
 
+//This code uses system call partAcount() to see how many times getpid() was called.
 int main(){
 	printf(1, "getpid() was called %d times\n", partACount());
 	exit();
