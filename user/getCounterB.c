15af319355f9862b56e8a4c3e180fb332d369a9b
Jman7615 <55512981+Jman7615@users.noreply.github.com>
2021-09-14 18:15:51 -0400

    comments for getCounterB

--- user/getCounterB.c
+++ user/getCounterB.c
@@ -1,6 +1,7 @@
 #include "types.h"
 #include "user.h"
 
+//This function uses system call partBCount() to get the number of system calls
 int main(){
 	printf(1, "There have been %d system calls\n", partBCount());
 	exit();
